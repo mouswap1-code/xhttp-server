5c34085f402cb7c425d3959d79789186fbe3b20b
mouswap1-code <mouswap1@gmail.com>
2026-05-19 01:36:28 +0100

    Configuration Upsun

---
+++ server.js
@@ -0,0 +1,6 @@
+const http = require('http');
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('XHTTP OK\n');
+});
+server.listen(process.env.PORT || 8080, () => console.log('OK'));
